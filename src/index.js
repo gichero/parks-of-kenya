359492457b0914c3bee14604eb859209411085d3
Amos <amosgathungu@gmail.com>
2017-09-06 13:29:45 -0400

    dependencies, components added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Redux from 'redux';
+import * as ReactRedux from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import {Router, Route, Link, IndexLink, IndexRoute, hashHistory} from 'react-router';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+
+const store = Redux.createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  Redux.compose(
+  Redux.applyMiddleware(ReduxThunk),
+  autoRehydrate()
+  )
+);
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
